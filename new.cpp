e19521c4b580b542d3b00e7cc446f95067f9132f
Badhon-herok <badhonherok@gmail.com>
2025-09-14 16:04:14 +0600

    saving in branch

---
+++ new.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int sum = 0;
+    for (int i = 1; i <= 500; i += 3) {
+        sum += i;
+    }
+    cout << "The sum of numbers from 1 to 500 with a difference of 3 is: " << sum << endl;
+    return 0;
+}
